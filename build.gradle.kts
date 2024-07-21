cf3c7b099b46b27901233d22cd7ccb0bc0a6b5ec
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-07-21 20:11:41 +1000

    Remove no longer required plugin in root build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    // Required for NeoGradle
-    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
     id("fabric-loom") version "1.7-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "1.0.13" apply false
 }
