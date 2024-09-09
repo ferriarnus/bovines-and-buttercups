6ef2825b1a8ee083bf93439543ed598bae865eef
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-09-09 16:05:38 +1000

    Update dependencies.

--- buildSrc/src/main/kotlin/house/greenhouse/bovinesandbuttercups/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/house/greenhouse/bovinesandbuttercups/gradle/Versions.kt
@@ -8,17 +8,17 @@ object Versions {
     const val NEOFORM = "$MINECRAFT-20240808.144430"
     const val PARCHMENT = "2024.07.28"
 
-    const val FABRIC_API = "0.102.1+1.21.1"
-    const val FABRIC_LOADER = "0.16.2"
+    const val FABRIC_API = "0.103.0+1.21.1"
+    const val FABRIC_LOADER = "0.16.4"
     const val MOD_MENU = "11.0.1"
     const val FABRIC_LOADER_RANGE = ">=0.15"
     const val FABRIC_MINECRAFT_RANGE = ">=1.21-"
 
-    const val NEOFORGE = "21.1.22"
+    const val NEOFORGE = "21.1.45"
     const val NEOFORGE_LOADER_RANGE = "[4,)"
     const val NEOFORGE_MINECRAFT_RANGE = "[1.21,)"
 
-    const val FABRIC_MIXIN = "0.15.0+mixin.0.8.7"
+    const val FABRIC_MIXIN = "0.15.3+mixin.0.8.7"
     const val MIXIN_EXTRAS = "0.4.1"
 
     const val JAVA = 21
