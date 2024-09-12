4536a31dddf73bff06a8434533fe6738c50b8f31
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-09-12 22:34:50 +1000

    Increase Absorption effect duration on Rich Honey Bottle.

--- common/src/main/java/house/greenhouse/bovinesandbuttercups/util/BovinesFoods.java
+++ common/src/main/java/house/greenhouse/bovinesandbuttercups/util/BovinesFoods.java
@@ -8,7 +8,7 @@ public class BovinesFoods {
     public static final FoodProperties RICH_HONEY_BOTTLE = new FoodProperties.Builder()
             .nutrition(6)
             .saturationModifier(0.3F)
-            .effect(new MobEffectInstance(MobEffects.ABSORPTION, 600, 0), 1.0F)
+            .effect(new MobEffectInstance(MobEffects.ABSORPTION, 800, 0), 1.0F)
             .alwaysEdible()
             .build();
 }
