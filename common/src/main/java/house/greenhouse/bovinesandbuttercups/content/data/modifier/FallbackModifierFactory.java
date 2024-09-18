26d0efe17a53a0ae74e36f89fb302b6c1b33dbe1
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-09-19 07:02:04 +1000

    Optimise imports.

--- common/src/main/java/house/greenhouse/bovinesandbuttercups/content/data/modifier/FallbackModifierFactory.java
+++ common/src/main/java/house/greenhouse/bovinesandbuttercups/content/data/modifier/FallbackModifierFactory.java
@@ -1,32 +1,13 @@
 package house.greenhouse.bovinesandbuttercups.content.data.modifier;
 
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import house.greenhouse.bovinesandbuttercups.BovinesAndButtercups;
 import house.greenhouse.bovinesandbuttercups.api.cowtype.modifier.NoOpTextureModifier;
 import house.greenhouse.bovinesandbuttercups.api.cowtype.modifier.TextureModifierFactory;
-import house.greenhouse.bovinesandbuttercups.network.clientbound.SyncConditionedTextureModifier;
-import house.greenhouse.bovinesandbuttercups.network.clientbound.SyncCowTypeClientboundPacket;
-import house.greenhouse.bovinesandbuttercups.registry.BovinesLootContextParamSets;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.ValidationContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.WeakHashMap;
 
 public class FallbackModifierFactory extends TextureModifierFactory<NoOpTextureModifier> {
     public static final MapCodec<FallbackModifierFactory> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
